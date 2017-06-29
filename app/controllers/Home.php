04f6eb64871f56a000c89440b3cc63135f3333ef
kkorsch <kkorsch7@gmail.com>
2017-06-29 20:53:44 +0200

    home/film method and view

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -19,5 +19,42 @@ class Home extends Controller
     $this->view('home/index', $filmy);
   }
 
-  
+  public function film(string $slug)
+  {
+    if (isset($slug) && !empty($slug)) {
+      $today = date('Y-m-d');
+      $nextWeek = date('Y-m-d', strtotime($today.'+1week'));
+      $db = new Database;
+
+      //check if film exists and select from database
+      $query = $db->prepare("SELECT * FROM films WHERE slug=:slug AND finish>=:today AND start<=:nextWeek");
+      if ($query->execute([
+        ':slug' => $slug,
+        ':today' => $today,
+        ':nextWeek' => $nextWeek,
+        ]) && $query->rowCount()) {
+        $film = $query->fetchObject();
+        /*$days = [];
+
+        //list of show days
+        if ($today < $film->start) {
+          $daysCount = (strtotime($today.'+6days')-strtotime($film->start))/86400;
+          $start = $film->start;
+        } elseif ($today >= $film->start && $today < $film->finish) {
+          $daysCount = (strtotime($film->finish)-strtotime($today))/86400;
+          if ($daysCount > 6) $daysCount = 6;
+          $start = $today;
+        } else {
+          $daysCount = 0;
+          $start = $today;
+        }
+
+        for ($i=0; $i<=$daysCount; $i++) {
+          $day = date('Y-m-d, l', strtotime($start.'+'.$i.'days'));
+          $days[$i] = $day;
+        }*/
+      }
+    }
+    $this->view('home/film', $film);
+  }
 }
---
+++ app/views/home/film.php
@@ -0,0 +1,34 @@
+<?php
+include('../app/views/partials/header.php');
+ ?>
+
+ <div class="row">
+   <h1 class="text-center">Moje Kino</h1>
+   <br>
+    <?php if (empty($data)): ?>
+      <h2 class="text-center">Nie znaleziono filmu</h2>
+    <?php else: ?>
+      <h3 class="text-center"> <strong><?php echo htmlspecialchars($data->title); ?></strong> </h3>
+      <hr>
+      <h4 class="text-center"> <strong>Opis</strong> </h4>
+      <p class="text-center"><?php echo htmlspecialchars($data->description); ?></p>
+      <hr>
+      <h4 class="text-center">Rezerwacja</h4>
+        <?php if (false)://(!empty($days)): ?>
+        <form class="col-md-offset-5 col-md-3" action="shows.php" method="get">
+          <input type="hidden" name="film" value="<?php echo $film->id; ?>">
+          <div class="form-group">
+            <label class="control-label">Dni</label>
+            <?php foreach ($days as $day): ?>
+              <button type="radio" name="date" value="<?php echo date('Y-m-d', strtotime($day)); ?>"class="form-control btn btn-default"><?php echo $day; ?></button>
+            <?php endforeach; ?>
+          </div>
+        </form>
+      <?php else: ?>
+        <p class="text-center">Brak seansów</p>
+      <?php endif;
+    endif; ?>
+ </div>
+
+<?php
+include('../app/views/partials/footer.php');
