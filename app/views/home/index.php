41eaa296898520a78a5c980857746910eba0932c
kkorsch <kkorsch7@gmail.com>
2017-06-29 20:05:11 +0200

    update home/index and delete example strona

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,24 @@
-<a href="home/strona">kliknij</a>
+<?php
+include('../app/views/partials/header.php');
+ ?>
+
+      <div class="row">
+        <h1 class="text-center">Moje Kino</h1>
+        <h4 class="text-center">Repertuar</h4>
+        <br><br>
+        <?php //if ($films): ?>
+        <ul class="list-group">
+          <?php //foreach($films as $film):  ?>
+            <form class="col-md-offset-5 col-md-3" action="film.php" method="get">
+              <button class="list-group-item list-group-item-info" type="submit" name="film" value="<?php //echo htmlspecialchars($film['slug']); ?>"><?php //echo htmlspecialchars($film['title']); ?></button>
+              <hr>
+
+            </form>
+          <?php //endforeach; ?>
+        </ul>
+      <?php //else: ?>
+        <h3 class="text-center">Brak filmów</h3>
+      <?php //endif; ?>
+      </div>
+<?php
+  include('../app/views/partials/footer.php');
